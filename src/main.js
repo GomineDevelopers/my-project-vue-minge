cec89e72a9debe9e20e72161e9177a22b7d39779
RC816 <caiyitian816@126.com>
2018-05-22 10:45:13 +0800

    update header

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import { Carousel,CarouselItem,Tabs,TabPane,Switch} from 'element-ui';
+import vueHeadful from 'vue-headful';
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.component('vue-headful', vueHeadful);
 Vue.use(Carousel);
 Vue.use(CarouselItem);
 Vue.use(Tabs);
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <vue-headful
+      title="时政要闻"
+    />
     <el-carousel :interval="5000" arrow="never">
       <el-carousel-item v-for="item in 4" :key="item">
         <h3>{{ item }}</h3>
