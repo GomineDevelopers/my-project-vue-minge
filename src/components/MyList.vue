7358ac2d1601cd4daaf4c4eba11d8fa941b19d7b
RC816 <caiyitian816@126.com>
2018-05-22 17:07:30 +0800

    update

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -2,8 +2,12 @@
   <scroller
     :on-infinite="infinite">
     <!-- content goes here -->
-    <div v-for="(item, index) in items" class="row" :class="{'grey-bg': index % 2 == 0}">
-      {{ item }}
+    <div v-for="(item, index) in items" class="row">
+      <div class="image"><img src="http://via.placeholder.com/350x350"></div>
+      <div class="text">
+        <div class="title">民革中央和最高检在上海开展联合调研</div>
+        <div class="content">5月12日至14日，万鄂湘率民革中央、最高人民检察院联合调研组赴上海开展调研。高小玫、张伯军参加调研。</div>
+      </div>
     </div>
   </scroller>
 </template>
@@ -27,7 +31,7 @@
       this.bottom = 20
     },
     methods: {
-      infinite (done) {
+      infinite(done) {
         setTimeout(() => {
           var start = this.bottom + 1
           for (var i = start; i < start + 10; i++) {
@@ -42,5 +46,49 @@
 </script>
 
 <style scoped>
+  .row {
+    margin: 0 2%;
+    padding: .9vh 0;
+    height: 11vh;
+    border-bottom: 1px solid #d3dce6;
+  }
 
+  .row .image {
+    height: 100%;
+    width: 30%;
+    float: left;
+  }
+
+  .row .image img {
+    height: 100%;
+    width: 100%;
+  }
+
+  .row .text {
+    width: 68%;
+    margin-left: 2%;
+    height: 100%;
+    line-height: 100%;
+    float: left;
+    text-align: justify;
+  }
+
+  .row .title {
+    width: 100%;
+    max-height: 6vh;
+    line-height: 3vh;
+    margin-top: .7vh;
+    overflow: hidden;
+  }
+
+  .row .content {
+    width: 100%;
+    height: 3vh;
+    line-height: 3vh;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 14px;
+    margin-top: .6vh;
+  }
 </style>
