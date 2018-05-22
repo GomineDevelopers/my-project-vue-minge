34156090d9fe7fedaaefd26b3bb1f8d731392a11
RC816 <caiyitian816@126.com>
2018-05-22 11:14:07 +0800

    update

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -9,13 +9,16 @@
       </el-carousel-item>
     </el-carousel>
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="市委会动态" name="first">
+      <el-tab-pane  name="first">
+        <span slot="label"> 市委会动态</span>
         1
       </el-tab-pane>
-      <el-tab-pane label="喜讯" name="second">
+      <el-tab-pane  name="second">
+        <span slot="label"> 喜讯</span>
         2
       </el-tab-pane>
-      <el-tab-pane label="专题报告" name="third">
+      <el-tab-pane  name="third">
+        <span slot="label"> 专题报告</span>
         3
       </el-tab-pane>
     </el-tabs>
@@ -27,7 +30,7 @@
     name: 'News',
     data() {
       return {
-        activeName: 'second'
+        activeName: 'first'
       }
     },
     methods: {
@@ -77,26 +80,37 @@
     width: 33.33%;
     color: #A4C3E4;
     height: 100%;
+    line-height: 100%;
+    vertical-align: top;
   }
 
+  .el-tabs__item span{
+    position: absolute;
+    line-height: 4vh;
+    width: 100%;
+    left: 0;
+    top: 50%;
+    height: 4vh;
+    margin-top: -2vh;
+  }
   .el-tabs__item.is-active {
     color: white;
     font-weight: bold;
   }
 
-  .el-tabs__item.is-active::after {
+  .el-tabs__item.is-active span::after {
     content: "";
     background: white;
     height: 5px;
     width: 5px;
     position: absolute;
-    bottom: 13%;
+    bottom: -2px;
     left: 50%;
     margin-left: -2.5px;
   }
 
   .el-tabs__active-bar {
-    display: none;
+    height: 0;
   }
 
   .el-tabs--top .el-tabs__item.is-top:nth-child(2) {
