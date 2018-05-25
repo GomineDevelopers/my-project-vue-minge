ef5c0abdce686623d6ef4c6fd39e8447524e2a51
shaossss <969348836@qq.com>
2018-05-25 18:09:39 +0800

    update

--- src/components/MyCommentList.vue
+++ src/components/MyCommentList.vue
@@ -19,7 +19,7 @@
                     <el-collapse accordion>
                       <el-collapse-item>
                         <template slot="title">
-                          共10条回复
+                          共10条回复&nbsp;<i class="header-icon el-icon-d-arrow-right"></i>
                         </template>
                         <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
                         <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
@@ -133,10 +133,7 @@
     padding-bottom: 0;
     font-size: 10px;
   }
-   .is-active{
-      background-color: #f0f5f9;
-    }
-    .is-active:active{
-      background-color: #f0f5f9;
-    }
+  .el-icon-arrow-right{
+     display: none;
+   }
   </style>
