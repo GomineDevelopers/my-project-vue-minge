add8dea2852e51494162ae50b45a8b5ad7253a66
shaossss <969348836@qq.com>
2018-06-01 13:53:28 +0800

    update

--- src/pages/Proposal.vue
+++ src/pages/Proposal.vue
@@ -1,38 +1,48 @@
 <template>
   <div class="bg1 proposal-cover">
+    <vue-headful title="提案" />
     <div class="proposal-list">
-      <div class="row status status-success">
-        <div class="note">提案1</div>
-        <div class="block">
-          <div class="title">本次党建工作十分重要大家要积极参与。</div>
-          <div class="content">民革贵阳市委召开八届三次会议</div>
-          <div class="c1"></div>
-          <div class="c2"></div>
-          <div class="c3">已通过</div>
-        </div>
-      </div>
-      <div class="row status status-uncheck">
-        <div class="note">提案2</div>
-        <div class="block">
-          <div class="title">本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。</div>
-          <div class="content">培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵</div>
-          <div class="c1"></div>
-          <div class="c2"></div>
-          <div class="c3">审核中</div>
-        </div>
-      </div>
-      <div class="row status status-wait">
-        <div class="note">提案3</div>
-        <div class="block">
-          <div class="title">本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。</div>
-          <div class="content">培训会上，陈应副主委希望大家重视宣传和 作，发挥好培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵</div>
-          <div class="btn-row"><span class="edit"><i class="el-icon-edit-outline"></i>修改</span><span class="delete"><i class="el-icon-delete"></i>删除</span>
+      <scroller
+        :on-infinite="infinite" class="scroller-container" >
+        <!-- content goes here -->
+          <div style="height: 44px;"></div>
+          <div class="row status" :class="[item.type == 1 ? 'status-success':[item.type == 2 ? 'status-uncheck':'status-wait']]" v-for="item in listData">
+            <div class="note" v-text="item.name"></div>
+            <div class="block">
+              <div class="title" v-text="item.title"></div>
+              <div class="content" v-text="item.content"></div>
+              <div class="btn-row" v-if="item.type == 3"><span class="edit"><i class="el-icon-edit-outline"></i>修改</span><span class="delete"><i class="el-icon-delete"></i>删除</span>
+              </div>
+              <div class="c1"></div>
+              <div class="c2"></div>
+              <div class="c3" v-if="item.type == 1">已通过</div>
+              <div class="c3" v-else-if="item.type == 2">审核中</div>
+              <div class="c3" v-else-if="item.type == 3">待提交</div>
+            </div>
           </div>
-          <div class="c1"></div>
-          <div class="c2"></div>
-          <div class="c3">待提交</div>
-        </div>
-      </div>
+          <!--<div class="row status status-uncheck">
+            <div class="note">提案2</div>
+            <div class="block">
+              <div class="title">本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。</div>
+              <div class="content">培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵</div>
+              <div class="c1"></div>
+              <div class="c2"></div>
+              <div class="c3">审核中</div>
+            </div>
+          </div>
+          <div class="row status status-wait">
+            <div class="note">提案3</div>
+            <div class="block">
+              <div class="title">本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。</div>
+              <div class="content">培训会上，陈应副主委希望大家重视宣传和 作，发挥好培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵</div>
+              <div class="btn-row"><span class="edit"><i class="el-icon-edit-outline"></i>修改</span><span class="delete"><i class="el-icon-delete"></i>删除</span>
+              </div>
+              <div class="c1"></div>
+              <div class="c2"></div>
+              <div class="c3">待提交</div>
+            </div>
+          </div>-->
+      </scroller>
     </div>
     <el-button type="primary" round class="bottom-btn">填写提案</el-button>
   </div>
@@ -42,7 +52,32 @@
   export default {
     name: "proposal",
     data() {
-      return {}
+      return {
+        isLast:false,
+        listData:[{"name":"提案1","title":"本次党建工作十分重要大家要积极参与。","content":"民革贵阳市委召开八届三次会议","type":1},
+                  {"name":"提案2","title":"本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。","content":"培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵","type":2},
+                  {"name":"提案3","title":"本次党建工作十分重要大家要积极参与。本次党建工作十分重要大家要积极参与。。","content":"民革贵阳市委召开八届三次会议","type":3},
+                  {"name":"提案4","title":"本次党建工作十分重要大家要积极参与333333333。","content":"培训会上，陈应副主委希望大家重视宣传和 作，发挥好培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵","type":1},
+                  {"name":"提案5","title":"本次党建工作十分重要大家要积极参与4444444444444。","content":"民革贵阳市委召开八届三次会议,培训会上，陈应副主委希望大家重视宣传和 作，发挥好培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵","type":3},
+                  {"name":"提案6","title":"本次党建工作十分重55555555555。","content":"民革贵阳市委召开八届三次会议,培训会上，陈应副主委希望大家重视宣传和 作，发挥好培训会上，陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵","type":3},
+                  {"name":"提案7","title":"本次党建工作十分66666666。","content":"民革贵阳市委召开八","type":2},
+                  {"name":"提案8","title":"本次党建工作十分7777777774。","content":"陈应副主委希望大家重视宣传和 作，发挥好各支部的宣传的阵","type":3},
+                  ]
+      }
+    },
+    created:function () {},
+    methods:{
+      infinite:function (done) {
+        let vm = this;
+        if(!vm.isLast){
+          vm.getListData(done);
+        }else{
+          done(true);
+        }
+      },
+      getListData:function () {},
+      del:function () {},
+      reWrite:function () {}
     }
 
   }
