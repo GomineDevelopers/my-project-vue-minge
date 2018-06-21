b9e732b97232ac551b871a38d80be01b8aa1d3ce
arronZhu <1274294199@qq.com>
2018-06-21 14:49:57 +0800

    提案审核

--- src/pages/CheckRead.vue
+++ src/pages/CheckRead.vue
@@ -1,13 +1,33 @@
 <template>
+ <div class="center_no_bg">
+     <el-row>
+       <el-col :span="24">
+           <div class="book-wrap ">
+               <div class="book">
+                  <img src="../../static/image/book-default.png" alt="" srcset="">
+               </div>
+               
+           </div>
+       </el-col>
+     </el-row>
+     <el-row>
+       <el-col :span="24"></el-col>
+     </el-row>
+     <el-row>
+       <el-col :span="24"></el-col>
+     </el-row>
+ </div>
 
 </template>
 
 <script>
-    export default {
-        name: "check-read"
-    }
+export default {
+  name: 'check-read'
+}
 </script>
 
 <style scoped>
-
+.book-wrap {
+  border: 1px solid #;
+}
 </style>
