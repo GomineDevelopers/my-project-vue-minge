7abbba1780cad8e2c793326d63e308d232f34799
arronZhu <1274294199@qq.com>
2018-06-22 09:48:00 +0800

    update

--- src/pages/CheckRead.vue
+++ src/pages/CheckRead.vue
@@ -12,7 +12,7 @@
      </el-row>
      <el-row>
        <el-col :span="24">
-         <div class="invite-message">
+         <div class="invite-message comma">
            <span>您好，您的好友</span>
            <span class="colored-text">张传一</span>
            <span>正在邀请您一
@@ -24,7 +24,7 @@
      <el-row>
           <el-col :span="24" class="proposal-button-wrap">
               <el-button type="primary" size="medium">通过</el-button>
-              <el-button class="color-button" type="" plain size="medium">拒绝</el-button>
+              <el-button class="color-button" type="primary" plain size="medium">拒绝</el-button>
           </el-col>
         </el-row>
  </div>
@@ -64,10 +64,30 @@ export default {
 }
 .invite-message {
   color: #333333;
-  width: 80%;
+  width: 70%;
   margin: 3vh auto;
 }
 .invite-message .colored-text {
   color: #dc9705;
+}
+.comma ::before {
+  content: '“';
+  font-size: 50px;
+  color: #ccc;
+  border-radius: 4px;
+  position: absolute;
+  z-index: 1000;
+  left: 6vh;
+  top: 0px;
+}
+.comma ::after {
+  content: '”';
+  font-size: 50px;
+  color: #ccc;
+  border-radius: 4px;
+  position: absolute;
+  z-index: 1000;
+  right: 6vh;
+  bottom: -4vh;
 }
 </style>
