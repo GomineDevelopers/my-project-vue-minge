8101b6c8f3d2ceae81e4e5ef952382c88d3cb361
shaossss <969348836@qq.com>
2018-06-22 16:51:07 +0800

    update

--- src/pages/read/NotesCenterDetail.vue
+++ src/pages/read/NotesCenterDetail.vue
@@ -1,5 +1,55 @@
 <template>
+  <div class="NCD_container">
+    <div class="NCD_topContainer">
+      <div class="title">《明朝那些事儿》</div>
+      <div class="chapter">第九章：决战不可避免</div>
+      <div class="content">在这段时间里，朱元璋做出的两个决策使得他成为了最终的战争胜利者，第一个决策是高筑墙、广积粮、缓称王，正是这个决定让他避开了天下</div>
+    </div>
+    <div class="NCD_bottomContainer">
+      <el-row>
+          <span class="NCD_bottomContainer-span-icon1"><i class="el-icon-view"></i>&nbsp;<span>1212</span></span>
+        <span class="NCD_bottomContainer-span-icon2"><i class="el-icon-edit-outline"></i>&nbsp;留言</span>
+      </el-row>
 
+      <div class="NCD_bottomContainer_commentArea">
+        <div class="NCD_bottomContainer_commentAreaDiv1">
+          <el-row>
+            <el-col :span="3" class="avatar"><img src="../../../static/image/book-default.png"></el-col>
+            <el-col :span="21">
+              <span class="userName">借东西的人</span>
+              <div class="commentContent">座谈中的内容结合自身实际，进一步激发老百姓脱贫的内生动力。</div>
+              <el-row>
+                <el-col :span="20">
+                  <span class="NCD_bottomContainer_commentArea_span">05-22 13:29</span>
+                </el-col>
+                <el-col :span="4">
+                  <span class="NCD_bottomContainer_commentArea_span"><i class="el-icon-edit-outline"></i>&nbsp;回复</span>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="NCD_bottomContainer_commentAreaDiv2">
+          <el-row>
+            <el-col :span="3" class="avatar"><img src="../../../static/image/book-default.png"></el-col>
+            <el-col :span="21">
+              <span class="userName">借东西的人</span>
+              <div class="commentContent">座谈中的内容结合自身实际，进一步激发老百姓脱贫的内生动力。</div>
+              <el-row>
+                <el-col :span="20">
+                  <span class="NCD_bottomContainer_commentArea_span">05-22 13:29</span>
+                </el-col>
+                <el-col :span="4">
+                  <span class="NCD_bottomContainer_commentArea_span"><i class="el-icon-edit-outline"></i>&nbsp;回复</span>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +59,89 @@
 </script>
 
 <style scoped>
+  .NCD_container{
+    margin: 3vh;
+  }
+
+  .NCD_topContainer{
+    background-color: white;
+    box-shadow: 0px 0px 20px 5px #e9e9e9;
+    border-top: 3px solid #419ddc;
+  }
+
+  .title{
+    color: #457094;
+    font-size: 1.2rem;
+    height: 6vh;
+    line-height: 6vh;
+    padding-top: 3vh;
+  }
+
+  .chapter{
+    color:#457094;
+    height: 6vh;
+    line-height: 6vh;
+  }
+
+  .content{
+    text-align: left;
+    padding: 0 3vh 2vh 3vh;
+  }
+
+  .NCD_bottomContainer{
+    margin-top: 3vh;
+  }
+
+  .NCD_bottomContainer_commentArea{
+    clear: both;
+    border: 1px dashed #cccccc;
+    border-radius: 5px;
+    padding:10px;
+    text-align: left;
+  }
+
+  .NCD_bottomContainer_commentAreaDiv1{
+    padding: 10px 0;
+  }
+
+  .NCD_bottomContainer_commentAreaDiv2{
+    padding: 10px 0;
+    border-top: 1px solid #cccccc;
+  }
+
+  .avatar img {
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+  }
+
+  .userName{
+    font-size: 12px;
+  }
+
+  .commentContent{
+    font-size: 14px;
+    padding: 5px 0;
+  }
+
+  .NCD_bottomContainer-span-icon1 {
+    float: left;
+    padding-bottom: 10px;
+    color: #999999;
+    font-size: 14px;
+  }
+
+  .NCD_bottomContainer-span-icon2 {
+    float: right;
+    padding-bottom: 10px;
+    color: #0064ba;
+    font-size: 14px;
+  }
+
+  .NCD_bottomContainer_commentArea_span{
+    font-size: 12px;
+    color: #cccccc;
+  }
+
 
 </style>
--- src/pages/read/ShareReadCenter.vue
+++ src/pages/read/ShareReadCenter.vue
@@ -1,5 +1,131 @@
 <template>
-  <div>3</div>
+  <div class="container">
+      <div class="card">
+        <el-row type="flex" justify="space-between">
+          <el-col :span="6">
+            <div class="bookImg"><img src="static/image/book-default.png"></div>
+          </el-col>
+          <el-col :span="10" class="book-container">
+            <div class="bookTitle">地图中的历史</div>
+            <el-row class="book-center">
+              <el-col :span="12">
+                <div class="authorDiv">
+                  <div class="author">王小波</div>
+                </div>
+              </el-col>
+              <el-col :span="12">
+                <div class="translate">
+                  <img src="../../../static/image/interpriter-icon.png">
+                  <span>王小波</span>
+                </div>
+              </el-col>
+            </el-row>
+            <div class="publish">天地人和出版社</div>
+          </el-col>
+          <el-col :span="6" class="reader">
+            <div class="reader-container">
+              <div class="readerPerson">读书人</div>
+              <div class="readerName">长一一</div>
+              <div class="readerTime">2017-07-21</div>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
+    <div class="card">
+      <el-row type="flex" justify="space-between">
+        <el-col :span="6">
+          <div class="bookImg"><img src="static/image/book-default.png"></div>
+        </el-col>
+        <el-col :span="10" class="book-container">
+          <div class="bookTitle">地图中的历史</div>
+          <el-row class="book-center">
+            <el-col :span="12">
+              <div class="authorDiv">
+                <div class="author">王小波</div>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="translate">
+                <img src="../../../static/image/interpriter-icon.png">
+                <span>王小波</span>
+              </div>
+            </el-col>
+          </el-row>
+          <div class="publish">天地人和出版社</div>
+        </el-col>
+        <el-col :span="6" class="reader">
+          <div class="reader-container">
+            <div class="readerPerson">读书人</div>
+            <div class="readerName">长一一</div>
+            <div class="readerTime">2017-07-21</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="card">
+      <el-row type="flex" justify="space-between">
+        <el-col :span="6">
+          <div class="bookImg"><img src="static/image/book-default.png"></div>
+        </el-col>
+        <el-col :span="10" class="book-container">
+          <div class="bookTitle">地图中的历史</div>
+          <el-row class="book-center">
+            <el-col :span="12">
+              <div class="authorDiv">
+                <div class="author">王小波</div>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="translate">
+                <img src="../../../static/image/interpriter-icon.png">
+                <span>王小波</span>
+              </div>
+            </el-col>
+          </el-row>
+          <div class="publish">天地人和出版社</div>
+        </el-col>
+        <el-col :span="6" class="reader">
+          <div class="reader-container">
+            <div class="readerPerson">读书人</div>
+            <div class="readerName">长一一</div>
+            <div class="readerTime">2017-07-21</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="card">
+      <el-row type="flex" justify="space-between">
+        <el-col :span="6">
+          <div class="bookImg"><img src="static/image/book-default.png"></div>
+        </el-col>
+        <el-col :span="10" class="book-container">
+          <div class="bookTitle">地图中的历史</div>
+          <el-row class="book-center">
+            <el-col :span="12">
+              <div class="authorDiv">
+                <div class="author">王小波</div>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="translate">
+                <img src="../../../static/image/interpriter-icon.png">
+                <span>王小波</span>
+              </div>
+            </el-col>
+          </el-row>
+          <div class="publish">天地人和出版社</div>
+        </el-col>
+        <el-col :span="6" class="reader">
+          <div class="reader-container">
+            <div class="readerPerson">读书人</div>
+            <div class="readerName">长一一</div>
+            <div class="readerTime">2017-07-21</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +135,124 @@
 </script>
 
 <style scoped>
+  .card{
+    margin: 2vh 3vh;
+    background-color: white;
+    box-shadow: 0px 0px 20px 5px #e9e9e9;
+    border-radius: 5px;
+  }
 
+  .card .bookImg{
+    margin: 1vh;
+    box-shadow: 0px 0px 20px 5px #e9e9e9;
+    border-radius: 5px;
+  }
+
+  .card .bookImg img{
+    width: 100%;
+    height: 100%;
+  }
+
+  .book-container{
+    display: flex;
+    flex-direction:column;
+    justify-content:center;
+  }
+
+  .book-center{
+    margin: 0 0 1vh 0;
+  }
+
+  .bookTitle{
+    text-align: left;
+    color: #457094;
+    font-weight: bold;
+    margin: 0 0 1.5vh 0;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .authorDiv{
+    height: 3vh;
+    line-height: 3vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .author{
+    width: 80%;
+    background-color: #e2edf5;
+    border-radius: 5px;
+    color: #457094;
+    font-size: .8rem;
+    padding: 3px 0;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .translate{
+    text-align: left;
+    height: 3vh;
+    line-height: 3vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .translate img{
+    width: 16px;
+    height:16px;
+  }
+
+  .translate span{
+    color: #8a969f;
+    font-size: .7rem;
+    display: inline-block;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .publish{
+    text-align: left;
+    color: #8a969f;
+    margin: 5px 0;
+    font-size: .7rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .reader{
+    display: flex;
+    flex-direction:column;
+    justify-content:center;
+  }
+
+  .reader-container{
+    background-color: #419ddc;
+    color: #ffffff;
+    font-size: .6rem;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    text-align: left
+  }
+  .readerName{
+    font-size:1rem ;
+    font-weight: bold;
+    border-top:1px dashed #ffffff ;
+    border-bottom:1px dashed #ffffff ;
+    padding: .5vh .85vh;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  .readerPerson{
+    padding: .5vh .85vh;
+    letter-spacing: 2px;
+  }
+  .readerTime{
+    padding: .5vh .85vh;
+  }
 </style>
