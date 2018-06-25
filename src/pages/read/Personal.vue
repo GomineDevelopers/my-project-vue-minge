bb7a7a2875e93fdcefb0ae9be0110eeb31263af7
arronZhu <1274294199@qq.com>
2018-06-25 10:44:55 +0800

    update

--- src/pages/read/Personal.vue
+++ src/pages/read/Personal.vue
@@ -1,16 +1,122 @@
 <template>
-  <div class="center_no_bg">
+  <div class="user center_no_bg">
     <vue-headful title="书友会"/>
-    <router-view></router-view>
+    <div class="read-center-wrap">
+      <div class="tab-wrapper" >
+          <div class="tab-item" @click="handleClick('first')" :class="{active:activeOne}">
+            <el-row>
+              <el-col :span="24"> <i class="iconfont icon-dushu"></i> </el-col>
+               <el-col :span="24"><span class="tab-text">书籍</span> </el-col>
+            </el-row>
+         
+            </div>
+          <div class="tab-item" @click="handleClick('second')" :class="{active:activeTwo}">
+              <el-row>
+                <el-col :span="24"> <i class="iconfont icon-jilu note"></i> </el-col>
+                <el-col :span="24"><span class="tab-text">笔记</span> </el-col>
+              </el-row>
+          </div>
+         </div>
+      </div>
+       <div class="router-view-wrapper">
+        <keep-alive>
+        <router-view/>
+        </keep-alive>
+        </div>
+      
+   
+    
   </div>
 </template>
 
 <script>
-    export default {
-        name: "personal"
+export default {
+  name: 'personal',
+  data() {
+    return {
+      activeOne: true,
+      activeTwo: false
     }
+  },
+  // mounted: function() {
+  //   this.$router.push({ name: 'BooksCenter' })
+  // },
+  methods: {
+    handleClick(flag) {
+      if (flag == 'first') {
+        this.activeOne = true
+        this.activeTwo = false
+        // this.$router.push({ name: 'BooksCenter' })
+      }
+
+      if (flag == 'second') {
+        this.activeOne = false
+        this.activeTwo = true
+        // this.$router.push({ name: 'NotesCenter' })
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
+.read-center-wrap {
+  position: absolute;
+  bottom: 0;
+  height: 8vh;
+  background: #ffffff;
+  width: 100%;
+  z-index: 999;
+  box-shadow: 0px -10px 10px #e9e9e9;
+}
+.active {
+  position: relative;
+}
+.active span {
+  color: #333333;
+}
+.active i {
+  color: #0064ba !important;
+}
 
+.tab-wrapper {
+  height: 8vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.tab-wrapper .tab-item {
+  display: flex;
+  align-items: center;
+  width: 50%;
+  height: 8vh;
+  color: #333333;
+  font-weight: bold;
+  justify-content: center;
+}
+.tab-item .icon-dushu {
+  color: #cccccc;
+  font-size: 1.4rem;
+}
+.tab-item .icon-jilu {
+  color: #cccccc;
+  font-size: 1.4rem;
+}
+.tab-item .tab-text {
+  font-size: 0.7rem;
+}
+.tab-wrapper .tab-item:first-child {
+  border-right: 1px solid #cccccc;
+}
+
+.router-view-wrapper {
+  height: 92vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
 </style>
+
+
