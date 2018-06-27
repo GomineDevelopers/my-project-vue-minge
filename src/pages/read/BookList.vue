2444a7d699e3b67e2e6d23691df4361de77e1fae
RC816 <caiyitian816@126.com>
2018-06-27 11:05:15 +0800

    update

--- src/pages/read/BookList.vue
+++ src/pages/read/BookList.vue
@@ -22,7 +22,7 @@
                 <div class="readingBookImg">
                   <img :src="item.img">
                   <div class="imgBottom"><span class="number">3</span>篇笔记</div>
-                  <div class="labelDiv" v-if="item.private_book == 1"><i class="el-icon-bell labelIcon"></i></div>
+                  <div class="labelDiv" v-if="item.private_book == 1"><i class="iconfont icon-lock labelIcon"></i></div>
                 </div>
               </el-col>
               <el-col :span="10" class="middleDiv">
@@ -48,7 +48,7 @@
               </el-col>
               <el-col :span="6">
                 <div class="time" v-text="$commonTools.formatDate(item.create_time)"></div>
-                <div class="note" @click.stop="writeNote">写笔记</div>
+                <div class="note" @click.stop="writeNote"><i class="iconfont icon-ymb"></i>写笔记</div>
               </el-col>
             </el-row>
           </div>
@@ -103,266 +103,272 @@
 </template>
 
 <script>
-export default {
-  name: 'book-list',
-  data() {
-    return {
-      activeName: 'first',
-      showReading: false,
-      showRead: false,
-      privateBookData: Array
-    }
-  },
-  mounted() {
-    this.getBookList()
-  },
-  methods: {
-    getBookList() {
-      let vm = this
-      vm
-        .axios(vm.$commonTools.g_restUrl, {
-          params: {
-            i: '8',
-            c: 'entry',
-            p: 'bookmates',
-            do: 'shop',
-            m: 'ewei_shop',
-            ac: 'private_book',
-            showLoading: true
-          }
-        })
-        .then(function(response) {
-          response.data.result.forEach(function(val, index, arr) {
-            if (val.status == 0) vm.showReading = true
-            else if (val.status == 1) vm.showRead = true
-            if (vm.showReading && vm.showRead) return false
+  export default {
+    name: 'book-list',
+    data() {
+      return {
+        activeName: 'first',
+        showReading: false,
+        showRead: false,
+        privateBookData: Array
+      }
+    },
+    mounted() {
+      this.getBookList()
+    },
+    methods: {
+      getBookList() {
+        let vm = this
+        vm
+          .axios(vm.$commonTools.g_restUrl, {
+            params: {
+              i: '8',
+              c: 'entry',
+              p: 'bookmates',
+              do: 'shop',
+              m: 'ewei_shop',
+              ac: 'private_book',
+              showLoading: true
+            }
           })
-          vm.privateBookData = response.data.result
-        })
-        .catch(function(error) {
-          console.log(error)
-        })
-    },
-    goBookDetail(id) {
-      this.$router.push({ name: 'BookDetail', params: { id: id } })
-    },
-    writeNote(id) {
-      this.$router.push({ name: 'AddNote', query: { bookId: id } })
+          .then(function (response) {
+            response.data.result.forEach(function (val, index, arr) {
+              if (val.status == 0) vm.showReading = true
+              else if (val.status == 1) vm.showRead = true
+              if (vm.showReading && vm.showRead) return false
+            })
+            vm.privateBookData = response.data.result
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      },
+      goBookDetail(id) {
+        this.$router.push({name: 'BookDetail', params: {id: id}})
+      },
+      writeNote(id) {
+        this.$router.push({name: 'AddNote', query: {bookId: id}})
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-/*无图书*/
-.no_bookImg img {
-  width: 60vw;
-}
+  /*无图书*/
+  .no_bookImg img {
+    width: 60vw;
+  }
 
-.no_bookDiv {
-  display: flex;
-  justify-content: center;
-}
+  .no_bookDiv {
+    display: flex;
+    justify-content: center;
+  }
 
-.no_bookText {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 10vh;
-  width: 90%;
-  padding: 5px;
-  position: relative;
-}
+  .no_bookText {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 10vh;
+    width: 90%;
+    padding: 5px;
+    position: relative;
+  }
 
-.comma::after {
-  content: '”';
-  font-size: 50px;
-  color: #ccc;
-  border-radius: 4px;
-  position: absolute;
-  z-index: 1000;
-  right: 0px;
-  bottom: -25px;
-}
+  .comma::after {
+    content: '”';
+    font-size: 50px;
+    color: #ccc;
+    border-radius: 4px;
+    position: absolute;
+    z-index: 1000;
+    right: 0px;
+    bottom: -25px;
+  }
 
-.comma::before {
-  content: '“';
-  font-size: 50px;
-  color: #ccc;
-  border-radius: 4px;
-  position: absolute;
-  z-index: 1000;
-  left: 0px;
-  top: 0px;
-}
+  .comma::before {
+    content: '“';
+    font-size: 50px;
+    color: #ccc;
+    border-radius: 4px;
+    position: absolute;
+    z-index: 1000;
+    left: 0px;
+    top: 0px;
+  }
 
-/*图书在读、已读*/
-.card {
-  /*width: 100%;*/
-  margin: 2vh 3vh;
-  background-color: white;
-  box-shadow: 0px 0px 20px 5px #e9e9e9;
-  border-radius: 5px;
-}
+  /*图书在读、已读*/
+  .card {
+    /*width: 100%;*/
+    margin: 2vh 3vh;
+    background-color: white;
+    box-shadow: 0px 0px 20px 5px #e9e9e9;
+    border-radius: 5px;
+  }
 
-.readingBook {
-  height: 80vh;
-  overflow-x: hidden;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
-}
+  .readingBook {
+    height: 80vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+  }
 
-.readingBook,
-.readBook {
-  text-align: left;
-}
+  .readingBook,
+  .readBook {
+    text-align: left;
+  }
 
-.readingBookImg img,
-.readBookImg img {
-  margin: 1vh;
-  box-shadow: 0px 0px 20px 5px #e9e9e9;
-  border-radius: 5px;
-  width: 100%;
-  max-height: 15vh;
-}
+  .readingBookImg img,
+  .readBookImg img {
+    margin: 1vh;
+    box-shadow: 0px 0px 20px 5px #e9e9e9;
+    border-radius: 5px;
+    width: 100%;
+    max-height: 15vh;
+  }
 
-.imgBottom {
-  position: absolute;
-  bottom: 10px;
-  margin: 0 1vh;
-  width: 25%;
-  font-size: 0.7rem;
-  background-color: rgba(255, 255, 255, 0.7);
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-  color: #457094;
-  text-align: center;
-  letter-spacing: 1px;
-}
+  .imgBottom {
+    position: absolute;
+    bottom: 10px;
+    margin: 0 1vh;
+    width: 25%;
+    font-size: 0.7rem;
+    background-color: rgba(255, 255, 255, 0.7);
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    color: #457094;
+    text-align: center;
+    letter-spacing: 1px;
+  }
 
-.number {
-  color: #eaa41e;
-}
+  .number {
+    color: #eaa41e;
+  }
 
-.labelDiv{
-  position: absolute;
-  border:17px solid;
-  border-color:#ec8947 transparent transparent #ec8947;
-  left:1vh;
-  top: 1vh;
-}
+  .labelDiv {
+    position: absolute;
+    border: 17px solid;
+    border-color: #ec8947 transparent transparent #ec8947;
+    left: 1vh;
+    top: 1vh;
+  }
 
-.labelDiv .labelIcon{
-  position: absolute;
-  top:-14px;
-  left:-15px;
-}
+  .labelDiv .labelIcon {
+    position: absolute;
+    top: -14px;
+    left: -15px;
+    color: #ffffff;
+    font-size: 12px;
+  }
 
-.middleDiv {
-  display: flex;
-  align-items: center;
-}
+  .middleDiv {
+    display: flex;
+    align-items: center;
+  }
 
-.middleDiv2 {
-  width: 100%;
-}
+  .middleDiv2 {
+    width: 100%;
+  }
 
-.bookTitle {
-  text-align: left;
-  color: #457094;
-  font-weight: bold;
-  margin: 0 0 1.5vh 0;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
+  .bookTitle {
+    text-align: left;
+    color: #457094;
+    font-weight: bold;
+    margin: 0 0 1.5vh 0;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
 
-.authorDiv {
-  display: flex;
-  height: 1.5rem;
-  line-height: 1.5rem;
-}
+  .authorDiv {
+    display: flex;
+    height: 1.5rem;
+    line-height: 1.5rem;
+  }
 
-.author {
-  background-color: #e2edf5;
-  border-radius: 5px;
-  color: #457094;
-  font-size: 0.8rem;
-  padding: 0 5px;
-  width: 60px;
-  height: 1.5rem;
-  line-height: 1.5rem;
-  text-align: center;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
+  .author {
+    background-color: #e2edf5;
+    border-radius: 5px;
+    color: #457094;
+    font-size: 0.8rem;
+    padding: 0 5px;
+    width: 60px;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    text-align: center;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
 
-.translate {
-  width: 50%;
-  margin-left: 5px;
-  text-align: left;
-  display: flex;
-  align-items: center;
-}
+  .translate {
+    width: 50%;
+    margin-left: 5px;
+    text-align: left;
+    display: flex;
+    align-items: center;
+  }
 
-.translate img {
-  width: 16px;
-  height: 16px;
-}
+  .translate img {
+    width: 16px;
+    height: 16px;
+  }
 
-.translate span {
-  color: #8a969f;
-  font-size: 0.7rem;
-  display: inline-block;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
+  .translate span {
+    color: #8a969f;
+    font-size: 0.7rem;
+    display: inline-block;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
 
-.publish {
-  width: 100%;
-  color: #8a969f;
-  font-size: 0.7rem;
-  height: 1.5rem;
-  line-height: 1.5rem;
-  padding-top: 0.5rem;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
+  .publish {
+    width: 100%;
+    color: #8a969f;
+    font-size: 0.7rem;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    padding-top: 0.5rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
 
-.progress span {
-  color: #8a969f;
-  font-size: 0.7rem;
-  float: left;
-  height: 3vh;
-  line-height: 3vh;
-}
+  .progress span {
+    color: #8a969f;
+    font-size: 0.7rem;
+    float: left;
+    height: 3vh;
+    line-height: 3vh;
+  }
 
-.time {
-  position: absolute;
-  right: 10px;
-  top: 12px;
-  color: #8a969f;
-  font-size: 0.7rem;
-}
+  .time {
+    position: absolute;
+    right: 10px;
+    top: 12px;
+    color: #8a969f;
+    font-size: 0.7rem;
+  }
 
-.note {
-  height: 30px;
-  line-height: 30px;
-  background-color: #419ddc;
-  color: #ffffff;
-  border-radius: 5px;
-  text-align: center;
-  padding: 0 5px;
-  position: absolute;
-  top: 50%;
-  margin-top: -15px;
-  right: 10px;
-  font-size: 0.9rem;
-  padding: 2px 10px;
-}
+  .note {
+    height: 30px;
+    line-height: 30px;
+    background-color: #419ddc;
+    color: #ffffff;
+    border-radius: 5px;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    margin-top: -15px;
+    right: 10px;
+    font-size: 0.9rem;
+    padding: 2px 10px;
+  }
+
+  .icon-ymb {
+    font-size: 12px;
+    margin-right: 3px;
+  }
 </style>
 
