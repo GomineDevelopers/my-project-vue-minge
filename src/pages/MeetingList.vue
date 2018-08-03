8a48d3603197cb1587f5947f996cd6801304b277
arronZhu <1274294199@qq.com>
2018-08-03 14:59:40 +0800

    把我的图书改为可移动按钮，增加只能半小时签到提醒

--- src/pages/MeetingList.vue
+++ src/pages/MeetingList.vue
@@ -55,314 +55,331 @@
 </template>
 
 <script>
-    export default {
-      name: "MeetingList",
-      data() {
-        return {
-          isLast: false,
-          listData: [],
-          curPage: 1,
-          dialogVisible: false,
-          dialogVisibleMembers:false,
-          isManager:false,
-          dialogContent:'',
-          detailMemberList:[]
-        }
-      },
-      created(){
-        this.getUserGrade();
-      },
-      methods: {
-        getUserGrade: function () {
-          let vm = this;
-          vm.axios(vm.$commonTools.g_restUrl, {
-              params: {
-                i: '8',
-                c: 'entry',
-                p: 'user',
-                do: 'shop',
-                m: 'ewei_shop',
-                ac: 'get_remind'
-              }
-            })
-            .then(function (response) {
-              if(response.data.result.grade == 0){
-                vm.isManager = true;
-              }
-            })
-            .catch(function (error) {
-              console.log(error)
-            })
-        },
-        infinite: function(done) {
-          let vm = this
-          if (!vm.isLast) {
-            vm.getListData(done)
-          } else {
-            done(true)
-          }
-        },
-        getListData: function(done) {
-          let vm = this;
-          this.axios(vm.$commonTools.g_restUrl, {
-            params: {
-              i: '8',
-              c: 'entry',
-              p: 'mq',
-              do: 'shop',
-              m: 'ewei_shop',
-              ac:'meeting_list',
-              page: vm.curPage
-            }
-          })
-            .then(function(response) {
-              if (response.data.result) {
-                for (var i = 0; i < response.data.result.list.length; i++) {
-                  if(response.data.result.list[i].address.substring(0,8) == 'https://' || response.data.result.list[i].address.substring(0,7) == 'http://'){
-                    response.data.result.list[i].isUrl = true;
-                  }else{
-                    response.data.result.list[i].isUrl = false;
-                  }
-                  vm.listData.push(response.data.result.list[i]);
-                }
-              }
-              if (response.data.result.list && response.data.result.list.length == 10) {
-                vm.isLast = false
-                vm.curPage++
-              } else {
-                vm.isLast = true
-              }
-              done()
-            })
-            .catch(function(error) {
-              console.log(error)
-            })
-        },
-        goDetail:function(item){
-          let vm =  this;
-          if(item.status == 0 || item.status == 1){
-            vm.dialogVisible = true;
-            vm.dialogContent = item;
-          }
-        },
-        changeStatus:function(id,status){
-          let vm = this;
-          this.axios(vm.$commonTools.g_restUrl, {
-            params: {
-              i: '8',
-              c: 'entry',
-              p: 'mq',
-              do: 'shop',
-              m: 'ewei_shop',
-              ac:'meeting_check',
-              id:id,
-              status:status
-            }
-          })
-            .then(function(response) {
-              if(response.data.status == 200){
-                vm.dialogVisible = false;
-                vm.$nextTick(function() {
-                  vm.listData = [];
-                  vm.curPage = 1;
-                  vm.isLast = false;
-                  vm.$refs.scroller.finishInfinite(false);
-                })
-              }else if(response.data.status == 0){
-                vm.dialogVisible = false;
-                vm.$message({showClose: true,message: '该条消息已过期，请刷新页面重试',type: 'error'});
-              }
-            })
-            .catch(function(error) {
-              console.log(error)
-            })
-
-        },
-        meetingDetail:function(id){
-          let vm = this;
-          this.axios(vm.$commonTools.g_restUrl, {
-            params: {
-              i: '8',
-              c: 'entry',
-              p: 'mq',
-              do: 'shop',
-              m: 'ewei_shop',
-              ac:'meeting_detail',
-              id:id
-            }
-          })
-            .then(function(response) {
-              vm.dialogVisibleMembers = true;
-              response.data.result.forEach(function (ele,value,arr) {
-                if(ele.status == 0){
-                  ele.status = "未处理";
-                }else if(ele.status == 1){
-                  ele.status = "参加";
-                }else if(ele.status == 2){
-                  ele.status = "拒绝";
-                }else if(ele.status == 3){
-                  ele.status = "已签到";
-                }else if(ele.status == 4){
-                  ele.status = "已过期";
-                }
-
-                if(ele.update_time == null || ele.update_time == ''){
-                  ele.update_time = '无';
-                }else{
-                  ele.update_time = vm.$commonTools.formatHour(ele.update_time);
-                }
-              })
-              vm.detailMemberList = response.data.result;
-            })
-            .catch(function(error) {
-              console.log(error)
-            })
-
-        },
-        addMeeting: function() {
-          this.$router.push({ name: 'AddMeeting' })
-        }
-      }
+export default {
+  name: 'MeetingList',
+  data() {
+    return {
+      isLast: false,
+      listData: [],
+      curPage: 1,
+      dialogVisible: false,
+      dialogVisibleMembers: false,
+      isManager: false,
+      dialogContent: '',
+      detailMemberList: []
     }
+  },
+  created() {
+    this.getUserGrade()
+  },
+  methods: {
+    getUserGrade: function() {
+      let vm = this
+      vm
+        .axios(vm.$commonTools.g_restUrl, {
+          params: {
+            i: '8',
+            c: 'entry',
+            p: 'user',
+            do: 'shop',
+            m: 'ewei_shop',
+            ac: 'get_remind'
+          }
+        })
+        .then(function(response) {
+          if (response.data.result.grade == 0) {
+            vm.isManager = true
+          }
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    infinite: function(done) {
+      let vm = this
+      if (!vm.isLast) {
+        vm.getListData(done)
+      } else {
+        done(true)
+      }
+    },
+    getListData: function(done) {
+      let vm = this
+      this.axios(vm.$commonTools.g_restUrl, {
+        params: {
+          i: '8',
+          c: 'entry',
+          p: 'mq',
+          do: 'shop',
+          m: 'ewei_shop',
+          ac: 'meeting_list',
+          page: vm.curPage
+        }
+      })
+        .then(function(response) {
+          if (response.data.result) {
+            for (var i = 0; i < response.data.result.list.length; i++) {
+              if (
+                response.data.result.list[i].address.substring(0, 8) ==
+                  'https://' ||
+                response.data.result.list[i].address.substring(0, 7) ==
+                  'http://'
+              ) {
+                response.data.result.list[i].isUrl = true
+              } else {
+                response.data.result.list[i].isUrl = false
+              }
+              vm.listData.push(response.data.result.list[i])
+            }
+          }
+          if (
+            response.data.result.list &&
+            response.data.result.list.length == 10
+          ) {
+            vm.isLast = false
+            vm.curPage++
+          } else {
+            vm.isLast = true
+          }
+          done()
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    goDetail: function(item) {
+      let vm = this
+      if (item.status == 0 || item.status == 1) {
+        vm.dialogVisible = true
+        vm.dialogContent = item
+      }
+    },
+    changeStatus: function(id, status) {
+      let vm = this
+      this.axios(vm.$commonTools.g_restUrl, {
+        params: {
+          i: '8',
+          c: 'entry',
+          p: 'mq',
+          do: 'shop',
+          m: 'ewei_shop',
+          ac: 'meeting_check',
+          id: id,
+          status: status
+        }
+      })
+        .then(function(response) {
+          if (response.data.status == 200) {
+            vm.dialogVisible = false
+            vm.$nextTick(function() {
+              vm.listData = []
+              vm.curPage = 1
+              vm.isLast = false
+              vm.$refs.scroller.finishInfinite(false)
+            })
+          } else if (response.data.status == 0) {
+            vm.dialogVisible = false
+            vm.$message({
+              showClose: true,
+              message: '该条消息已过期，请刷新页面重试',
+              type: 'error'
+            })
+          } else if (response.data.status == 2) {
+            vm.dialogVisible = false
+            vm.$message({
+              showClose: true,
+              message: '还未到签到时间，请刷新页面确认',
+              type: 'error'
+            })
+          }
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    meetingDetail: function(id) {
+      let vm = this
+      this.axios(vm.$commonTools.g_restUrl, {
+        params: {
+          i: '8',
+          c: 'entry',
+          p: 'mq',
+          do: 'shop',
+          m: 'ewei_shop',
+          ac: 'meeting_detail',
+          id: id
+        }
+      })
+        .then(function(response) {
+          vm.dialogVisibleMembers = true
+          response.data.result.forEach(function(ele, value, arr) {
+            if (ele.status == 0) {
+              ele.status = '未处理'
+            } else if (ele.status == 1) {
+              ele.status = '参加'
+            } else if (ele.status == 2) {
+              ele.status = '拒绝'
+            } else if (ele.status == 3) {
+              ele.status = '已签到'
+            } else if (ele.status == 4) {
+              ele.status = '已过期'
+            }
+
+            if (ele.update_time == null || ele.update_time == '') {
+              ele.update_time = '无'
+            } else {
+              ele.update_time = vm.$commonTools.formatHour(ele.update_time)
+            }
+          })
+          vm.detailMemberList = response.data.result
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    addMeeting: function() {
+      this.$router.push({ name: 'AddMeeting' })
+    }
+  }
+}
 </script>
 
 <style scoped>
-  .scroller-container {
-    top: -25px !important;
-    padding-bottom: 44px;
-  }
+.scroller-container {
+  top: -25px !important;
+  padding-bottom: 44px;
+}
 
-  .proposal-cover {
-    height: 100vh;
-    position: relative;
-  }
+.proposal-cover {
+  height: 100vh;
+  position: relative;
+}
 
-  .proposal-cover .proposal-list {
-    height: 88vh;
-    position: relative;
-    overflow: hidden;
-  }
+.proposal-cover .proposal-list {
+  height: 88vh;
+  position: relative;
+  overflow: hidden;
+}
 
-  .proposal-cover .proposal-full-list {
-    height: 96vh !important;
-  }
+.proposal-cover .proposal-full-list {
+  height: 96vh !important;
+}
 
-  .proposal-cover .proposal-list .row {
-    width: 88%;
-    margin: auto;
-  }
+.proposal-cover .proposal-list .row {
+  width: 88%;
+  margin: auto;
+}
 
-  .proposal-cover .proposal-list .row .block {
-    text-align: left;
-    background: #ffffff;
-    margin-top: 5px;
-    margin-bottom: 10px;
-    padding-bottom: 10px;
-    border-left: 3px solid #56b0f0;
-    box-shadow: 0px 0px 20px 5px #e9e9e9;
-    position: relative;
-  }
+.proposal-cover .proposal-list .row .block {
+  text-align: left;
+  background: #ffffff;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-left: 3px solid #56b0f0;
+  box-shadow: 0px 0px 20px 5px #e9e9e9;
+  position: relative;
+}
 
-  .proposal-cover .proposal-list .row .block .title {
-    height: 30px;
-    line-height: 30px;
-    border-bottom: 1px solid #cccccc;
-    color: #80919c;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-    font-size: 14px;
-    padding: 5px 60px 5px 8px;
-  }
+.proposal-cover .proposal-list .row .block .title {
+  height: 30px;
+  line-height: 30px;
+  border-bottom: 1px solid #cccccc;
+  color: #80919c;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 14px;
+  padding: 5px 60px 5px 8px;
+}
 
-  .proposal-cover .proposal-list .row .block .content {
-    line-height: 25px;
-    padding: 5px 60px 5px 8px;
-  }
+.proposal-cover .proposal-list .row .block .content {
+  line-height: 25px;
+  padding: 5px 60px 5px 8px;
+}
 
-  .proposal-cover .proposal-list .row .block .btn-row {
-    text-align: right;
-    color: #2192e0;
-    font-size: 14px;
-    height: 30px;
-    line-height: 30px;
-    margin-right: 10px;
-  }
+.proposal-cover .proposal-list .row .block .btn-row {
+  text-align: right;
+  color: #2192e0;
+  font-size: 14px;
+  height: 30px;
+  line-height: 30px;
+  margin-right: 10px;
+}
 
-  .proposal-cover .bottom-btn {
-    position: absolute;
-    bottom: 4vh;
-    left: 50%;
-    width: 200px;
-    margin-left: -100px;
-    padding: 12px 0;
-    letter-spacing: 1px;
-    box-shadow: 0px 0px 20px 5px #e9e9e9;
-  }
+.proposal-cover .bottom-btn {
+  position: absolute;
+  bottom: 4vh;
+  left: 50%;
+  width: 200px;
+  margin-left: -100px;
+  padding: 12px 0;
+  letter-spacing: 1px;
+  box-shadow: 0px 0px 20px 5px #e9e9e9;
+}
 
-  .status .c1 {
-    width: 0;
-    height: 0;
-    border-left: 60px solid transparent;
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
+.status .c1 {
+  width: 0;
+  height: 0;
+  border-left: 60px solid transparent;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 
-  .status .c2 {
-    width: 0;
-    height: 0;
-    border-top: 20px solid white;
-    border-left: 20px solid transparent;
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
+.status .c2 {
+  width: 0;
+  height: 0;
+  border-top: 20px solid white;
+  border-left: 20px solid transparent;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 
-  .status .c3 {
-    top: 12px;
-    right: -11px;
-    color: white;
-    position: absolute;
-    width: 60px;
-    height: 20px;
-    transform: rotate(45deg);
-    z-index: 111;
-    font-size: 12px;
-    text-align: center;
-    letter-spacing: 0.5px;
-  }
+.status .c3 {
+  top: 12px;
+  right: -11px;
+  color: white;
+  position: absolute;
+  width: 60px;
+  height: 20px;
+  transform: rotate(45deg);
+  z-index: 111;
+  font-size: 12px;
+  text-align: center;
+  letter-spacing: 0.5px;
+}
 
-  .status-uncheck .c1 {
-    border-top: 60px solid #cccccc;
-  }
+.status-uncheck .c1 {
+  border-top: 60px solid #cccccc;
+}
 
-  .status-join .c1 {
-    border-top: 60px solid #4798c5;
-  }
+.status-join .c1 {
+  border-top: 60px solid #4798c5;
+}
 
-  .status-refuse .c1 {
-    border-top: 60px solid #E6A23C;
-  }
+.status-refuse .c1 {
+  border-top: 60px solid #e6a23c;
+}
 
-  .status-success .c1 {
-    border-top: 60px solid #66c661;
-  }
+.status-success .c1 {
+  border-top: 60px solid #66c661;
+}
 
-  .status-overdue .c1 {
-    border-top: 60px solid #d26e7a;
-  }
+.status-overdue .c1 {
+  border-top: 60px solid #d26e7a;
+}
 
-  .dialog_content{
-    text-align: left;
-    margin: 0.8vh 0;
-    word-wrap: break-word;
-  }
+.dialog_content {
+  text-align: left;
+  margin: 0.8vh 0;
+  word-wrap: break-word;
+}
 
-  .contentDiv{
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-
+.contentDiv {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
 </style>
