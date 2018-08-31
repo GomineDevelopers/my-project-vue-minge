827e8152432ed7cc4578950d2c5862434a27c1c2
shaossss <969348836@qq.com>
2018-08-31 17:22:37 +0800

    update

--- src/pages/application/six.vue
+++ src/pages/application/six.vue
@@ -80,7 +80,6 @@
           let vm = this;
           let submitData = JSON.parse(decodeURI(vm.$commonTools.getCookie("cookieData")));
           submitData.img = vm.postImgName;
-          console.info(submitData)
           vm.axios(vm.$commonTools.g_restUrl, {
               method: 'post',
               params: {
